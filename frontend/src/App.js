b700a5643e629d1234d534881e613518eb63ba64
Hafsa <it23168268@my.sliit.lk>
2025-03-23 14:45:31 +0530

    add frontend folders and files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RegisterLogin from './pages/user-dashboard/register-login/RegisterLogin';
+
+
+function App() {
+  return (
+    <div>
+      <RegisterLogin/>
+    </div>
+  );
+}
+
+export default App;
