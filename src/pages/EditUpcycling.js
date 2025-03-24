89a34de57e52e3afca14688d1aca0978d11966fb
yoniiiX <it23235038@my.sliit.lk>
2025-03-24 20:38:09 +0530

    update file uploaded

---
+++ src/pages/EditUpcycling.js
@@ -0,0 +1,15 @@
+import React,{useEffect,useState} from "react";
+import axios from 'axios'
+import {useParams}from 'react-router'
+import {useNavigate} from 'react-router'
+
+function EditUpcycling(){
+
+    
+    return(
+        <div>
+            <h1>Update User</h1>
+        </div>
+    )
+}
+export default EditUpcycling
