f1cec31a50a741dbdfbfe3806d9e593d9f821e37
Hafsa <it23168268@my.sliit.lk>
2025-03-25 00:02:40 +0530

    start coding your part here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import RegisterLogin from './pages/user-dashboard/register-login/RegisterLogin';
-
+import "./App.css";
+//import RegisterLogin from './pages/user-dashboard/register-login/RegisterLogin';
+//import your page here
 
 function App() {
   return (
     <div>
-      <RegisterLogin/>
+      {/* <RegisterLogin/> 
+      add it like the one above */}
+      <h1> hiiii</h1>
     </div>
   );
 }
