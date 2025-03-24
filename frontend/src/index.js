1f555e11c274dbd017eea644b9bfdbd437dad7fa
Hafsa <it23168268@my.sliit.lk>
2025-03-25 00:30:25 +0530

    fixed React 18 issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import from 'react-dom/client'
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter> 
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+// Create root
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
